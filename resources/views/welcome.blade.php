ce5aa8f35401ea2efef6df0ca98a20342c7ba6d6
tpid-laili <laili@technopartner.co.id>
2022-12-31 19:45:05 +0700

    first project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
